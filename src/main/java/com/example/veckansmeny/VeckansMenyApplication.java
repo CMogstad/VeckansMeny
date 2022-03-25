feb3515df898d3e21814ff695eaebf6c55df1ed9
strawberryPenny <93320042+strawberryPenny@users.noreply.github.com>
2022-03-25 10:20:58 +0100

    Initial commit

---
+++ src/main/java/com/example/veckansmeny/VeckansMenyApplication.java
@@ -0,0 +1,13 @@
+package com.example.veckansmeny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeckansMenyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeckansMenyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/veckansmeny/VeckansMenyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.veckansmeny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeckansMenyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
