7d4f413d0db84c7d39a43ddc18d8f1bd86d5df1b
strawberryPenny <93320042+strawberryPenny@users.noreply.github.com>
2022-03-25 10:40:32 +0100

    Added packages, dao interfaces and controller file.

---
+++ src/main/java/com/example/veckansmeny/controller/VeckansMenyController.java
@@ -0,0 +1,7 @@
+package com.example.veckansmeny.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class VeckansMenyController {
+}
---
+++ src/main/java/com/example/veckansmeny/dao/IngredientDao.java
@@ -0,0 +1,6 @@
+package com.example.veckansmeny.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientDao extends JpaRepository<Ingredient, Integer> {
+}
